d82f80c9be9b8a9f51481900764597d80005b9f9
Matthew Holman <mholman000@gmail.com>
2026-08-29 15:17:47 +0200

    intitial push

---
+++ apps/backend/medusa-config.ts
@@ -0,0 +1,30 @@
+import { loadEnv, defineConfig } from '@medusajs/framework/utils'
+import path from 'path'
+
+loadEnv(process.env.NODE_ENV || 'development', process.cwd())
+
+module.exports = defineConfig({
+  projectConfig: {
+    databaseUrl: process.env.DATABASE_URL,
+    redisUrl: process.env.REDIS_URL,
+    http: {
+      storeCors: process.env.STORE_CORS!,
+      adminCors: process.env.ADMIN_CORS!,
+      authCors: process.env.AUTH_CORS!,
+      jwtSecret: process.env.JWT_SECRET,
+      cookieSecret: process.env.COOKIE_SECRET,
+    }
+  },
+  admin: {
+    vite: () => {
+      return {
+        resolve: {
+          alias: {
+            react: path.resolve(__dirname, 'node_modules/react'),
+            'react-dom': path.resolve(__dirname, 'node_modules/react-dom'),
+          },
+        },
+      }
+    },
+  },
+})
